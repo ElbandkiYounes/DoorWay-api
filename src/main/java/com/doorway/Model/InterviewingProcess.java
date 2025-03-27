4b31cc4a5861400ee162aa4fb2bab9035754f69d
MouadIDK <mouad.idkaroum@edu.uiz.ac.ma>
2025-03-27 14:57:11 +0000

    set intervwee to null on intervweing process

--- src/main/java/com/doorway/Model/InterviewingProcess.java
+++ src/main/java/com/doorway/Model/InterviewingProcess.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.*;
+
 @EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
@@ -21,30 +24,33 @@ public class InterviewingProcess {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
     @Enumerated(EnumType.STRING)
     @Builder.Default
     private Decision decision = Decision.NEUTRAL;
+
     @Column(columnDefinition = "TEXT")
     private String feedback;
-
 
     @ManyToOne
     private Role role;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "interviewee_id", nullable = true)
+    @OnDelete(action = OnDeleteAction.SET_NULL)
     @JsonIgnore
     private Interviewee interviewee;
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
     @JsonIgnore
-    private List<Interview> interviews = Collections.emptyList();
+    private List<Interview> interviews = new ArrayList<>();
 
     @CreatedDate
     private LocalDateTime createdAt;
 
     @JsonProperty("intervieweeId")
     public UUID getIntervieweeId() {
-        return interviewee.getId();
+        return interviewee != null ? interviewee.getId() : null;
     }
 }
